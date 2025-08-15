87f0d68d715da5ac8441f59bf2a471958e7a14e8
vlpetrova <vlpetrova@abv.bg>
2025-08-16 00:04:38 +0200

    Add Foody files

---
+++ Foody/Models/ApiResponseDTO.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Text.Json.Serialization;
+
+namespace Foody.Models
+{
+    internal class ApiResponseDTO
+    {
+        [JsonPropertyName("msg")]
+        public string? Msg { get; set; }
+
+        [JsonPropertyName("foodId")]
+        public string? FoodId { get; set; }
+    }
+}
